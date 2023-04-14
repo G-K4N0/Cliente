c70b22e39b53a88ef16b80681e29660bda8b49b9
K4T0 <talentounido.kano@gmail.com>
2023-04-14 04:10:53 -0600

    App e index

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.scss'
 import { Login } from './components/login/Login'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Dashboard } from './components/dash/Dashboard'
-import { Avisos } from './components/Pages/Aviso/GetAviso'
+import { Aviso } from './components/Pages/Aviso/Aviso'
 import { Carreras } from './components/Pages/Carrera/Carreras'
 import { Estadisticas } from './components/Pages/Estadistica/Estadisticas'
 import { Home } from './components/Pages/Home'
@@ -11,23 +12,37 @@ import { Labs } from './components/Pages/Lab/Labs'
 import { Materias } from './components/Pages/Materia/Materias'
 import { Inicio } from './components/home/Inicio'
 import { Usuario } from './components/Pages/Usuario/Usuario'
+import { Grupos } from './components/Pages/Grupos/Grupos'
+import { NotFound } from './components/NotFound/NotFound'
+import RequireAuth from './context/RequireAuth'
+import PersistLogin from './context/PersistLogin'
+
 function App () {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" exact={true} element={<Inicio/>} />
+          <Route path="/" exact={true} element={<Inicio />} />
           <Route path="/login" exact={true} element={<Login />} />
-          <Route path="/dashboard" exact={true} element={<Dashboard />}>
-            <Route path="inicio" exact={true} element={<Home/>} />
-            <Route path="usuarios" exact={true} element={<Usuario/>} />
-            <Route path="materias"exact={true} element={<Materias/>} />
-            <Route path="carreras" exact={true} element={<Carreras/>} />
-            <Route path="labs" exact={true} element={<Labs/>} />
-            <Route path="horarios" exact={true} element={<Horarios/>} />
-            <Route path="avisos" exact={true} element={<Avisos/>} />
-            <Route path="estadisticas" exact={true} element={<Estadisticas/>} />
+
+          <Route element={<PersistLogin />}>
+            <Route element={<RequireAuth allowedRoles={[1]} />}>
+              <Route path="/dashboard" exact={true} element={<Dashboard />}>
+                <Route path="inicio" exact={true} element={<Home />} />
+                <Route path="usuarios" exact={true} element={<Usuario />} />
+                <Route path="materias" exact={true} element={<Materias />} />
+                <Route path="grupos" exact={true} element={<Grupos />}></Route>
+                <Route path="carreras" exact={true} element={<Carreras />} />
+                <Route path="labs" exact={true} element={<Labs />} />
+                <Route path="horarios" exact={true} element={<Horarios />} />
+                <Route path="avisos" exact={true} element={<Aviso />} />
+                <Route path="estadisticas" exact={true} element={<Estadisticas />}/>
+              </Route>
+            </Route>
           </Route>
+
+          <Route path="*" element={<NotFound />} />
+
         </Routes>
       </BrowserRouter>
     </div>
