7ab2823c01454aae5b093f46797aca10c58fc75f
K4T0 <talentounido.kano@gmail.com>
2023-04-14 04:09:54 -0600

    Cards y detalles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -5,21 +5,21 @@ export function Card (props) {
   return (
     <div id={props.id} className={styles.cardContainer}>
       <div className={styles.nameLab}>
-        <h1>{ props.laboratorio }</h1>
+        <h1>{props.laboratorio}</h1>
       </div>
       <div className={styles.materiaName}>
-        <h3>{ props.materia}</h3>
+        <h3>{props.materia}</h3>
       </div>
       <Details
-      inicio= {props.inicio}
-      fin= { props.fin }
-      grupo = { props.grupo }
-      semestre = { props.semestre }
-      status = { props.status }
-      usuario = { props.usuario }
-      imagen = { props.imagenv}
+        inicio={props.inicio}
+        fin={props.fin}
+        grupo={props.grupo}
+        semestre={props.semestre}
+        status={props.status}
+        usuario={props.usuario}
+        imagen={props.imagen}
       />
-      <h2 className={styles.cardCareer} >{ props.carrera }</h2>
+      <p className={styles.cardCareer}>{props.carrera}</p>
     </div>
   )
 }
--- src/components/Card/Details.jsx
+++ src/components/Card/Details.jsx
@@ -1,11 +1,13 @@
 import styles from '../styleComponents/Details.module.scss'
-
+import { Image } from 'react-bootstrap'
 export function Details (props) {
-  const imagen = ''
+  const imagen = props.imagen
   return (
-    <div className={styles.containerDetails}>
+    <div className={styles.containerDetails} >
       <div className={styles.containerImage}>
-        <img src={imagen} alt=""/>
+        <Image src={imagen}
+        roundedCircle
+        />
       </div>
       <div className= {styles.franja} ></div>
       <div className={styles.scheduleContainer}>
@@ -14,7 +16,7 @@ export function Details (props) {
       <div className={styles.hour}><h3>{ props.inicio }</h3></div>
       <h1 className={styles.scheduleTitle} >Termina</h1>
       <div className={styles.hour}><h3> { props.fin } </h3></div>
-      <div><h2 className={styles.scheduleTitle} > {props.semestre} </h2></div>
+      <div><p className={styles.scheduleTitle} > {props.semestre} </p></div>
       </div>
     </div>
   )
