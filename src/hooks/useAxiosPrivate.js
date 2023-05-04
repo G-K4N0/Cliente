f3c9c231493e8aa58fe67a9e13f68cf997ed4cea
K4T0 <talentounido.kano@gmail.com>
2023-05-04 17:58:18 -0600

    axios privado reestructurado

--- src/hooks/useAxiosPrivate.js
+++ src/hooks/useAxiosPrivate.js
@@ -1,37 +1,47 @@
 import useAuth from './useAuth'
 import { useEffect } from 'react'
 import { axiosPrivate } from '../services/AxiosPrivate.js'
-import useRefreshToken from './useRefreshToken'
+import useRefreshToken from './useRefreshToken.js'
 
 const useAxiosPrivate = () => {
   const refresh = useRefreshToken()
   const { auth } = useAuth()
 
   useEffect(() => {
-    const requestIntercept = axiosPrivate.interceptors.request.use(
-      (config) => {
-        if (!config.headers.Authorization) {
-          config.headers.Authorization = `Bearer ${auth?.token}`
-        }
-        return config
-      },
-      (error) => Promise.reject(error)
-    )
+    let requestIntercept
+    let responseIntercept
 
-    const responseIntercept = axiosPrivate.interceptors.response.use(
-      (response) => response,
-      async (error) => {
-        const prevRequest = error?.config
-        if (error?.response?.status === 403 && !prevRequest?.sent) {
-          prevRequest.sent = true
-          const newAccesToken = await refresh()
-          prevRequest.headers.Authorization = `Bearer ${newAccesToken}`
+    const configureAxiosInterceptors = () => {
+      requestIntercept = axiosPrivate.interceptors.request.use(
+        (config) => {
+          if (!config.headers.Authorization) {
+            config.headers.Authorization = `Bearer ${auth?.token}`
+          }
+          return config
+        },
+        (error) => Promise.reject(error)
+      )
 
-          return axiosPrivate(prevRequest)
+      responseIntercept = axiosPrivate.interceptors.response.use(
+        (response) => response,
+        async (error) => {
+          const prevRequest = error?.config
+          if (error?.response?.status === 403 && !prevRequest?.sent) {
+            prevRequest.sent = true
+            try {
+              const newAccessToken = await refresh()
+              prevRequest.headers.Authorization = `Bearer ${newAccessToken}`
+              return axiosPrivate(prevRequest)
+            } catch (error) {
+              return Promise.reject(error)
+            }
+          }
+          return Promise.reject(error)
         }
-        return Promise.eject(error)
-      }
-    )
+      )
+    }
+
+    configureAxiosInterceptors()
 
     return () => {
       axiosPrivate.interceptors.request.eject(requestIntercept)
