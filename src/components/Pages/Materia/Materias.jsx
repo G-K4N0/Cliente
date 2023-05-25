d756770eeeabac662bdc255d6bde7f20bcb404a2
K4T0 <talentounido.kano@gmail.com>
2023-05-25 10:29:26 -0600

    cambio de lugar de componente Agregar

--- src/components/Pages/Materia/Materias.jsx
+++ src/components/Pages/Materia/Materias.jsx
@@ -1,14 +1,11 @@
 import { Materia } from './Materia.jsx'
-import { Agregar } from './Agregar.jsx'
+
 export const Materias = () => {
   return (
-    <div className='container'>
-      <div className='container'>
-        <Agregar />
+    <div className="container">
+      <div>
+        <Materia />
       </div>
-    <div >
-      <Materia />
-    </div>
     </div>
   )
 }
