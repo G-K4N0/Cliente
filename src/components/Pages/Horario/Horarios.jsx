2d5c19cb391b422a4c0c641a70b7970af990dac3
K4T0 <talentounido.kano@gmail.com>
2023-05-25 10:35:26 -0600

    estilizacion de divs

--- src/components/Pages/Horario/Horarios.jsx
+++ src/components/Pages/Horario/Horarios.jsx
@@ -1,13 +1,14 @@
 import { Agregar } from './Agregar.jsx'
 import { Horario } from './Horario.jsx'
+import './Horarios.module.scss'
 export function Horarios () {
   return (
-    <div>
-      <div>
+    <div className='container'>
+      <div >
       <Agregar />
       </div>
 
-      <div>
+      <div className='container-custom-horarios'>
         <Horario />
       </div>
     </div>
