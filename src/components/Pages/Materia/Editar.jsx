e4688bfc340070cda144109d20b38bae4b92f2f5
K4T0 <talentounido.kano@gmail.com>
2023-05-25 10:36:55 -0600

    Desestructuración de Props

--- src/components/Pages/Materia/Editar.jsx
+++ src/components/Pages/Materia/Editar.jsx
@@ -1,14 +1,14 @@
 import { Modal, Button } from 'react-bootstrap'
 
-export const Editar = (props) => {
+export const Editar = ({ setNewValue, showModal, handleEdit, newValue, handleCancel }) => {
   const handleInputChange = (e) => {
     const newValue = e.target.value
-    props.setNewValue(newValue)
+    setNewValue(newValue)
   }
 
   return (
     <div>
-      <Modal show={props.showModal} onHide={() => props.handleEdit()}>
+      <Modal show={showModal} onHide={() => handleEdit()}>
         <Modal.Header closeButton>
           <Modal.Title>Editar materia</Modal.Title>
         </Modal.Header>
@@ -18,16 +18,16 @@ export const Editar = (props) => {
             <input
               type="text"
               name="name"
-              value={props.newValue}
+              value={newValue}
               onChange={handleInputChange}
             />
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={props.handleCancel}>
+          <Button variant="secondary" onClick={handleCancel}>
             Cancelar
           </Button>
-          <Button variant="danger" onClick={props.handleEdit}>
+          <Button variant="danger" onClick={handleEdit}>
             Editar
           </Button>
         </Modal.Footer>
