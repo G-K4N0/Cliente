79a4bd660d661966ccc6466c5650c3938002eff8
K4T0 <talentounido.kano@gmail.com>
2023-05-25 10:24:43 -0600

    validaciones

--- src/components/home/Inicio.jsx
+++ src/components/home/Inicio.jsx
@@ -4,17 +4,47 @@ import useAxiosPrivate from '../../hooks/useAxiosPrivate.js'
 import { useEffect, useState } from 'react'
 import { Card } from '../Card/Card'
 import { CurrentCard } from '../Card/CurrentCard'
+import { convertStringToTime } from '../../services/convertStringToTime'
+import { SuccessAlert } from '../Alerts/Success'
+import { ErrorAlert } from '../Alerts/Error'
+
 export function Inicio () {
   const axiosPrivate = useAxiosPrivate()
   const [horarios, setHorarios] = useState([])
   const [horario, setHorario] = useState(null)
   const [time, setTime] = useState('')
+  const [mensaje, setMensaje] = useState('')
+  const [showSucces, setShowSucces] = useState(false)
+  const [showError, setShowError] = useState(false)
 
   useEffect(() => {
     axiosPrivate
       .get('/horarios/dias')
       .then((response) => {
-        setHorarios(response.data)
+        if (Object.keys(response.data).length === 0) {
+          setMensaje('No hay datos para mostrar')
+          setShowSucces(true)
+        } else {
+          const horariosFiltrados = response.data.filter((horario) => {
+            return (
+              time > convertStringToTime(horario.inicia) &&
+              time < convertStringToTime(horario.finaliza)
+            )
+          })
+          setHorarios(horariosFiltrados)
+        }
+      })
+      .catch((error) => {
+        setMensaje(error)
+        setShowError(true)
+      })
+  }, [axiosPrivate, time])
+
+  useEffect(() => {
+    axiosPrivate
+      .get(`/horarios/one/${2}`)
+      .then((response) => {
+        setHorario(response?.data)
       })
       .catch((error) => {
         console.log(error)
@@ -22,50 +52,49 @@ export function Inicio () {
   }, [axiosPrivate])
 
   useEffect(() => {
-    axiosPrivate
-      .get(`/horarios/one/${2}`)
-      .then((response) => {
-        console.log(response.data)
-        setHorario(response.data)
-      })
-      .catch((error) => {
-        console.log(error)
-      })
-  }, [])
-
-  useEffect(() => {
     const interval = setInterval(() => {
-      const localTime = new Date()
+      /* const localTime = new Date()
       const horaActual =
-        localTime.getTime() + localTime.getTimezoneOffset() * 6000
-      const hora = new Date(horaActual + 3600000 * 18)
+        localTime.getTime() + localTime.getTimezoneOffset() * 6000 */
+      const hora = new Date()
       setTime(hora.toLocaleTimeString('es-MX'))
     }, 1000)
     return () => clearInterval(interval)
   }, [])
 
+  const elementos =
+    horarios.length > 0
+      ? (
+          horarios.map((horario, index) => (
+        <Card
+          key={horario.laboratorio + index}
+          laboratorio={horario.laboratorio}
+          materia={horario.materia}
+          inicio={horario.inicia}
+          fin={horario.finaliza}
+          grupo={horario.grupo}
+          status={horario.ocupado}
+          imagen={horario.image.url}
+          carrera={horario.carrera}
+          usuario={horario.docente}
+        />
+          ))
+        )
+      : (
+      <h1>No hay horarios en inicio</h1>
+        )
+
   return (
     <div>
       <BarraNavegacion login="Iniciar sesión" />
       <div className={`container-fluid ${styles.containerInicio}`}>
-        (<h1>{time}</h1>)
-        <div className={styles.cards}>
-          {horarios.map((horario) => (
-            <Card
-              key={horario.id}
-              id={horario.id}
-              laboratorio={horario.laboratorio}
-              materia={horario.materia}
-              inicio={horario.inicia}
-              fin={horario.finaliza}
-              grupo={horario.grupo}
-              status={horario.ocupado}
-              imagen={horario.image.url}
-              carrera={horario.carrera}
-              usuario={horario.docente}
-            />
-          ))}
-        </div>
+        <SuccessAlert
+          show={showSucces}
+          setShow={setShowSucces}
+          mensaje={mensaje}
+        />
+        <ErrorAlert show={showError} setShow={setShowError} error={mensaje} />
+        <div className={styles.cards}>{elementos}</div>
         <div className={styles.card}>
           {horario == null
             ? (
